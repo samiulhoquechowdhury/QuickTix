2205a320bd42def018b9735d240efb8a0b262bd8
samiulhoquechowdhury <samiulofficialworks@gmail.com>
2025-08-26 05:10:07 +0530

    navbar done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Movies from "./pages/Movies";
 import MovieDetails from "./pages/MovieDetails";
@@ -9,19 +9,23 @@ import MyBookings from "./pages/MyBookings";
 import Favorite from "./pages/Favorite";
 import Footer from "./components/Footer";
 
+import { Toaster } from "react-hot-toast";
+
 const App = () => {
+  const isAdminRoute = useLocation().pathname.startsWith("/admin");
   return (
     <>
-      <Navbar />
+      <Toaster />
+      {!isAdminRoute && <Navbar />}
       <Routes>
-        {/* <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
         <Route path="/movies/:id" element={<MovieDetails />} />
         <Route path="/movies/:id/:data" element={<SeatLayout />} />
         <Route path="/my-bookings" element={<MyBookings />} />
-        <Route path="/favourite" element={<Favorite />} /> */}
+        <Route path="/favourite" element={<Favorite />} />
       </Routes>
-      <Footer />
+      {!isAdminRoute && <Footer />}
     </>
   );
 };
